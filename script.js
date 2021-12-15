58c925c584fd41226009b2610bf4cfe57cf196e7
KabilanC <92292287+KabilanC@users.noreply.github.com>
2021-12-15 14:17:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function foo(foo) {
+    let a=document.getElementById('firstname').value
+    let b=document.getElementById('middlename').value
+    let c=document.getElementById('lastname').value
+    let d=document.getElementById('email').value
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(d);
+   }
+   
